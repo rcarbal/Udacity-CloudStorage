fe2c0fcec04cf3f77bdf03e9a95fe5d9cf7518ca
rcarbal <rcarbaleq2@gmail.com>
2021-01-15 06:07:32 -0800

    Add FileService and FileServiceController

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/FileServiceController.java
@@ -0,0 +1,31 @@
+package com.udacity.jwdnd.course1.cloudstorage.controller;
+
+import com.udacity.jwdnd.course1.cloudstorage.services.FileService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+@RequestMapping("files")
+public class FileServiceController {
+
+    private FileService fileService;
+
+    public FileServiceController(FileService fileService) {
+        this.fileService = fileService;
+    }
+
+    @GetMapping
+    public String getAllFiles(){
+        return fileService.getAllFiles();
+    }
+
+    @PostMapping
+    public String uploadFile(@RequestParam("fileUpload") MultipartFile file, Model model){
+        return fileService.addFile();
+    }
+}
---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/FileService.java
@@ -0,0 +1,11 @@
+package com.udacity.jwdnd.course1.cloudstorage.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FileService {
+
+    public String getAllFiles() {
+        return null;
+    }
+}
